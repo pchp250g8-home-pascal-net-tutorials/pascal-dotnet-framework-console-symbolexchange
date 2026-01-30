fd118a0a1cbd3e2a691c452d8494fb767c902ee2
Alexander <pchp250g8@tutamail.com>
2026-01-30 16:53:48 +0300

    Created Project SymbolExchange

---
+++ SymbolExchange.pas
@@ -0,0 +1,13 @@
+﻿uses
+  System;
+begin
+  Console.WriteLine('Input a string');
+  var strLine1 := Console.ReadLine();
+  var nLen := strLine1.Length;
+  var strLine2 := strLine1[nLen] + strLine1.Substring(1, nLen - 2) + 
+      strLine1[1];
+  Console.WriteLine('Exchange the first and the last symbol in the string');
+  Console.WriteLine('Source string:{0}', strLine1);
+  Console.WriteLine('Destinstion string:{0}', strLine2);
+  Console.Read();
+end.
